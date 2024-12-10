d6b6ea44e3731484c6a299f4bd5c15a8f91b602d
Oscar Antonio <92340250+EzzCream@users.noreply.github.com>
2024-12-09 18:49:24 -0600

    add

---
+++ TipoDeDato/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int edad = 23;
+        String nombre = "Oscar";
+        boolean bandera = true;
+        double estatura = 1.67;
+
+        System.out.println("Mi nombre es: " + nombre + ", tengo " + edad + " de edad y mido: " + estatura);
+
+    }
+}
